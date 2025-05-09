9da2502493055260036f68f61c43e9749895f6cb
davisricart <davisricart@gmail.com>
2025-05-09 19:01:56 -0400

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// firebase-config.js
-const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id"
-};
